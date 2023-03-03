ac00a2d5d428ca9661fc049b492073ebcd3ea46e
Yasmine <yhines004@gmail.com>
2023-03-03 03:01:07 -0600

    Update README and main.py

--- API/main.py
+++ API/main.py
@@ -4,12 +4,11 @@ import json,time
 app = Flask(__name__)
 
 @app.route('/', methods=['GET'])
-def home_page():
-    data_set ={'Message':'Automate all the things!','timestamp':time.time()}
-    json_dump = json.dumps(data_set)
+def home():
+    data = {'Message':'Automate all the things!','timestamp':time.time()}
+    json_dump = json.dumps(data)
 
     return json_dump
-
 
 if __name__ == '__main__':
     app.run(port=5849)
